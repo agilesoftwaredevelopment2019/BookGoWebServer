6d0b9d9223fb839409b3f601b724153aace6ceb1
chang02 <chang0022@snu.ac.kr>
2019-06-01 14:38:05 +0900

    vue-template

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app id="app">
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+      //
+    }
+  },
+  mounted () {
+    this.check()
+  },
+  methods: {
+    ...mapActions({
+      check: 'check'
+    })
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-layout column>
+    HomePage
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
