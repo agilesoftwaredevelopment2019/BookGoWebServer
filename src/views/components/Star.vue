8dce96c4882554173121900c3e03d4409105f627
12rics <jwp1994@naver.com>
2019-06-11 20:41:51 +0900

    star api

--- src/views/components/Star.vue
+++ src/views/components/Star.vue
@@ -12,26 +12,57 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'Star',
   data () {
     return {
+      isInterested: false
     }
   },
+  created () {
+    this.asdf()
+  },
   methods: {
-    toggleInterested: function () {
-      if (this.isInterested) {
-        this.$toast.info('관심상품이 해제되었습니다')
-      } else {
-        this.$toast.info('관심상품이 등록되었습니다')
+    async asdf () {
+      try {
+        let uid = this.$store.state.uid
+        let response = await axios.get('https://bookgo.herokuapp.com/interests/' + uid)
+        window.console.log(response)
+        const f = response.data.find((element) => {
+          return element.book_id === this.$store.state.item.uid
+        })
+        this.isInterested = f !== undefined
+      } catch (err) {
+        this.$toast.error('Failed to get data from server')
       }
-      this.isInterested = !this.isInterested
+    },
+    async toggleInterested () {
+      if (!this.$store.state.login) {
+        this.$router.push({ path: '/login' })
+      }
+      try {
+        if (this.isInterested) {
+          let uid = this.$store.state.uid
+          let bid = this.$store.state.item.book_id
+          await axios.delete('https://bookgo.herokuapp.com/interests/' + uid + '/' + bid)
+          this.$toast.info('관심상품이 해제되었습니다')
+        } else {
+          await axios.post('https://bookgo.herokuapp.com/interests', {
+            user_id: this.$store.state.uid,
+            book_id: this.$store.state.item.book_id
+          })
+          this.$toast.info('관심상품이 등록되었습니다')
+        }
+        this.isInterested = !this.isInterested
+      } catch (err) {
+        this.$toast.error('Failed to get data from server')
+      }
     }
   },
   components: {
   },
   props: {
-    isInterested: Boolean
   }
 }
 </script>
