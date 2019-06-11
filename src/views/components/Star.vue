b61e837d6ea81895f0f09cb1420641c3ca90d68d
12rics <jwp1994@naver.com>
2019-06-11 17:58:41 +0900

    toggleInterested function in Star.vue

--- src/views/components/Star.vue
+++ src/views/components/Star.vue
@@ -2,9 +2,9 @@
   <div>
     <v-container fluid ma-0 pa-0>
       <div class="star">
-        <v-btn icon large dark color="primary" v-on:click="flag = !flag">
-          <v-icon v-if=flag>star_border</v-icon>
-          <v-icon v-else>star</v-icon>
+        <v-btn icon large dark color="primary" v-on:click="toggleInterested">
+          <v-icon v-if=isInterested>star</v-icon>
+          <v-icon v-else>star_border</v-icon>
         </v-btn>
       </div>
     </v-container>
@@ -19,8 +19,19 @@ export default {
     }
   },
   methods: {
+    toggleInterested: function () {
+      if (this.isInterested) {
+        this.$toast.info('관심상품이 해제되었습니다')
+      } else {
+        this.$toast.info('관심상품이 등록되었습니다')
+      }
+      this.isInterested = !this.isInterested
+    }
   },
   components: {
+  },
+  props: {
+    isInterested: Boolean
   }
 }
 </script>
